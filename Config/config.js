87b047ff8af2f19b4e534829fc57be30c55ef108
fabricien-web <rakotoarimananafabricien@gmail.com>
2019-09-01 04:01:45 +0300

    aloka mainty

--- Config/config.js
+++ Config/config.js
@@ -1,6 +1,5 @@
 module.exports = {
     //url: 'mongodb://localhost:27017/simplontest'
-    //url:'mongodb+srv://fabricien:&&setra16022000cluster0-ab1xf.mongodb.net/test?retryWrites=true&w=majority'
-    url:" mongodb+srv://Fabicien:&&setra16022000@fabricien-ehlbw.mongodb.net/test"
+    url:'mongodb+srv://Fabicien:&&setra16022000@fabricien-ehlbw.mongodb.net/test?retryWrites=true&w=majority'
     
 }
